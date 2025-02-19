e7164e643e6eb91854737f328ec470a6248b9812
Fiona Bui <buiphf@gmail.com>
2025-02-19 10:31:11 +0100

    test env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+console.log(import.meta.env.VITE_TMDB_API_KEY);
+
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const api = axios.create({
